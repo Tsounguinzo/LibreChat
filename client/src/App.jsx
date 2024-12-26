f4befc094949da7bccdaa82889361f5cd092fdd6
Beaudelaire Tsoungui Nzodoumkouo <beaudelaire@tutamail.com>
2024-12-26 11:36:32 -0500

    added session recording

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,29 +25,29 @@ const App = () => {
   });
 
   return (
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <LiveAnnouncer>
-            <ThemeProvider>
-              <RadixToast.Provider>
-                <ToastProvider>
-                  <DndProvider backend={HTML5Backend}>
-                    <RouterProvider router={router} />
-                    <ReactQueryDevtools initialIsOpen={false} position="top-right" />
-                    <Toast />
-                    <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
-                  </DndProvider>
-                </ToastProvider>
-              </RadixToast.Provider>
-            </ThemeProvider>
-          </LiveAnnouncer>
-        </RecoilRoot>
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <LiveAnnouncer>
+          <ThemeProvider>
+            <RadixToast.Provider>
+              <ToastProvider>
+                <DndProvider backend={HTML5Backend}>
+                  <RouterProvider router={router} />
+                  <ReactQueryDevtools initialIsOpen={false} position="top-right" />
+                  <Toast />
+                  <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
+                </DndProvider>
+              </ToastProvider>
+            </RadixToast.Provider>
+          </ThemeProvider>
+        </LiveAnnouncer>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 };
 
 export default () => (
-    <ScreenshotProvider>
-      <App />
-    </ScreenshotProvider>
+  <ScreenshotProvider>
+    <App />
+  </ScreenshotProvider>
 );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,26 @@ import App from './App';
 import './style.css';
 import './mobile.css';
 import { ApiErrorBoundaryProvider } from './hooks/ApiErrorBoundaryContext';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+if (typeof window !== 'undefined') {
+  posthog.init('phc_lc8S8T8PkN3FuqqwIp9Yfw0pi53ik7dt7qOyWyQ2eC2', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'always',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
+    },
+  });
+}
+
 root.render(
   <ApiErrorBoundaryProvider>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </ApiErrorBoundaryProvider>,
 );
